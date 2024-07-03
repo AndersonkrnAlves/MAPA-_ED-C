ac3473d3003fe21b166b8ea7adb4f3c74b8a2053
AndersonkrnAlves <spfc-alves@hotmail.com>
2024-07-03 16:44:07 -0300

    vamos programar

--- vscode C/Listasdinamicas.c
+++ vscode C/Listasdinamicas.c
@@ -89,4 +89,44 @@ void lista_remover(ptr_no lista)
         atual = (ptr_no) malloc(sizeof(no));
         printf("\n\nEscolha uma dos itens: \n");
         scanf("%d", &dado);
+        while ((lista->dado != dado) )
+            {
+                if (lista->proximo == NULL)
+                    {
+                        break;
+                }
+                atual = lista;
+                lista = lista->proximo;
+        }
+        if (lista->dado == dado)
+            {
+                atual->proximo = lista->proximo;
+        }
 }
+
+//Desenha o conteúdo da Lista na tela
+void lista_mostrar(ptr_no lista) 
+    {
+        system("cls");
+        while (1)
+            {
+                printf("%d, ", lista->dado);
+                if (lista->proximo == NULL)
+                    {
+                        break;
+                }
+                lista = lista->proximo;
+        }
+}
+
+//Desenha conteúdo da Lista na tela
+void lista_mostrar_2(ptr_no lista) 
+    {
+        system("cls");
+        while (lista->proximo == NULL)
+            {
+                printf("%d, ", lista->dado);
+                lista = lista->proximo;
+        }
+        printf("%d, ", lista->dado);
+} 
